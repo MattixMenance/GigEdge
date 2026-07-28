86c018eb882e4b1e20e6d92135c359099a046748
MattixMenance <topflightabc@gmail.com>
2026-07-28 00:13:20 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gig-edge-v1";
+const CACHE_NAME = "gig-edge-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -9,15 +9,52 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      );
+
     })
+
   );
+
+  self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request).then(response => {
+
+      return response || fetch(event.request);
+
+    })
+
+  );
+
 });
